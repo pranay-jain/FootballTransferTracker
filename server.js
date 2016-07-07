e7421282a2e95af9abd55a6c0f7ab5d61301e9cb
Pranay Jain <pranay1117@yahoo.co.in>
2016-07-08 00:40:12 +0530

    Added MDL and browserify

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express'),
 
 var port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+	res.render('index');
+});	
+
 const server = app.listen(port, (req, res) => {
 	console.log("App running on port " + port);
 });
